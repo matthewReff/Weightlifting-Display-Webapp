1e1d93926a21e10e55b083465b9c00c6ca500919
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2023-11-24 20:20:26 -0600

    add error logger

--- packages/express-backend/src/index.ts
+++ packages/express-backend/src/index.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import helloApiRoutes from "./endpoints/hello";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send("Something on the server failed");
+});
+
 app.use("/hello", helloApiRoutes);
+
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
