1e3245eaeabb4f0534cca439b4b2f807a1f812fa
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2024-01-07 22:15:02 -0600

    Add components and toggle for graph types

--- apps/react-frontend/src/App.tsx
+++ apps/react-frontend/src/App.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 import MeasuredMaxGraphs from "./page-components/measured-max-graphs";
 import EstimatedMaxGraphs from "./page-components/estimated-max-graphs";
+import Select, { SelectOption } from "./components/select";
 
 export enum GraphsToDisplay {
   ESTIMATED_MAX = "ESTIMATED_MAX",
   MEASURED_MAX = "MEASURED_MAX"
 }
 function App() {
-  const [ selectedGraphsToDisplay, setSelectedGraphsToDisplay ] = useState<GraphsToDisplay>(GraphsToDisplay.MEASURED_MAX);
+  const [ selectedGraphsToDisplay, setSelectedGraphsToDisplay ] = useState<string>(GraphsToDisplay.MEASURED_MAX);
+  const selectOptions: SelectOption[] = [
+    {
+      value: GraphsToDisplay.MEASURED_MAX,
+      label: "Measured Max Lifts",
+    },
+    {
+      value: GraphsToDisplay.ESTIMATED_MAX,
+      label: "Estimated Max Lifts",
+    },
+  ];
 
   return (
     <div className="bg-background">
+      <Select
+        options={selectOptions}
+        onChange={e => {
+          setSelectedGraphsToDisplay(e.target.value);
+        }}
+      >
+      </Select>
       { selectedGraphsToDisplay === GraphsToDisplay.MEASURED_MAX &&
         <MeasuredMaxGraphs />
       }
---
+++ apps/react-frontend/src/components/select.tsx
@@ -0,0 +1,23 @@
+import Form from "react-bootstrap/esm/Form";
+import { FormSelectProps } from "react-bootstrap/esm/FormSelect";
+
+export interface SelectOption {
+  value: string,
+  label: string
+}
+export interface SelectProps extends FormSelectProps {
+  options: SelectOption[]
+}
+const Select = (props: SelectProps) => {
+  return (
+    <Form.Select
+      {...props}
+    >
+      {props.options.map((option) => {
+        return <option value={option.value}>{option.label}</option>
+      })}
+    </Form.Select>
+  )
+}
+
+export default Select;
